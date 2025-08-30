266ce71912e1e4985500c4c5cda2530478c94229
Daniel D <73590614+danieljdd@users.noreply.github.com>
2025-08-30 09:54:06 +0200

    testnet false

--- packages/widget/src/devMode/loadWidget.tsx
+++ packages/widget/src/devMode/loadWidget.tsx
@@ -11,7 +11,7 @@ import { Widget, WidgetProps } from "@/widget/Widget";
 const DevMode = () => {
   const [theme, setTheme] = useState<"dark" | "light">("dark");
   const [apiUrl, setApiUrl] = useState<"prod" | "dev">("prod");
-  const [testnet, setTestnet] = useState<boolean>(true);
+  const [testnet, setTestnet] = useState<boolean>(false);
   const [disableShadowDom, setDisableShadowDom] = useState(true);
   const [renderWebComponent, setRenderWebComponent] = useState(false);
 
